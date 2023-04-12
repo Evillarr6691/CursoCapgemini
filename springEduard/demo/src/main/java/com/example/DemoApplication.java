c5404b340102448d193607dc746d5801d4d46f7d
Eduard Villarroya Pérez <128491305+Evillarr6691@users.noreply.github.com>
2023-04-12 11:48:18 +0200

    Actualizando proxy

--- springEduard/demo/src/main/java/com/example/DemoApplication.java
+++ springEduard/demo/src/main/java/com/example/DemoApplication.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.domains.contracts.repositories.ActorRepository;
 import com.example.domains.contracts.services.ActorService;
@@ -27,11 +31,23 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	@Primary
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplateLB() {
+		return new RestTemplate();
+	}
+
 //	@Autowired
 //	ActorRepository dao;
 	
-	@Autowired
-	ActorService srv;
+//	@Autowired
+//	ActorService srv;
 	
 	@Override
 	@Transactional
