c40b475658b9ef63acf716d62fe6adfe30109709
Eduard Villarroya Pérez <eduard.villarroya-perez@capgemini.com>
2023-04-14 08:14:15 +0200

    arrow functions

--- springEduard/fundamentos/demo.js
+++ springEduard/fundamentos/demo.js
@@ -19,3 +19,19 @@ console.log(`---> fuera${d}`);
 // if ((a = 1)) {
 //   console.log(`Cierto`);
 // }
+
+
+function avg(){
+  let result = 0;
+  for(let i = 0; i<arguments.length; i++){
+    result += arguments[i];
+  }
+
+  return arguments.length ? (result / arguments.length) : 0;
+
+}
+
+console.log(avg(10, 20, 30));
+console.log(avg(...t));
+
+t.filter(item => item % 2);
