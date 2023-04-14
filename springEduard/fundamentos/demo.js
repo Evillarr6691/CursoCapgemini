b827f4f8cdf7849a1af4ff6cf9d35020edb0a6d8
Eduard Villarroya Pérez <eduard.villarroya-perez@capgemini.com>
2023-04-14 09:23:55 +0200

    probando objetos en js

--- springEduard/fundamentos/demo.js
+++ springEduard/fundamentos/demo.js
@@ -1,37 +1,131 @@
-a = 1;
-b = 2;
-c = a = b = 3;
-'use strict';
-console.log(`---> ${a + b}`);
+console.log('Soy el fichero en node')
+x = 'En demo'
+a = '5s'
+let b = 2
+c = a = b = 3
+var imprimir = false;
+// console.log(`-------> ${a+b}`)
 
 function kk() {
-  var a = "7";
-  if (true) {
-    var x = "bloque";
-  }
-
-  d = a + b;
-  console.log(`---> ${d}`);
+    let a = '7'
+    if (true) {
+        let x = 'bloque'
+    }
+    d = a + x;
+    console.log(`-------> ${d}`)
 }
-
 kk();
-console.log(`---> fuera${d}`);
-// if ((a = 1)) {
-//   console.log(`Cierto`);
+
+console.log(`-------> fuera ${d}`)
+
+function usaKK() {
+    kk();
+}
+// if(a=1) {
+//     console.log(`Cierto`)
+// }
+// inprimir = true
+// if(imprimir) {
+//     console.log(`Cierto`)
 // }
 
+t = [10, 20, 30]
+//t = { x: 10, y: 20}
+// for(v in t) {
+//     console.log(t[v])
+// }
+// for (v of t) {
+//     console.log(v)
+// }
+// cmp = 'x'
+// t[cmp]
+// t.x === t['x']
 
-function avg(){
-  let result = 0;
-  for(let i = 0; i<arguments.length; i++){
-    result += arguments[i];
-  }
+// let cond = 0;
 
-  return arguments.length ? (result / arguments.length) : 0;
+// console.log(cond ? `Cierto` : `Falso`)
 
+// function avg() {
+//     var rslt = 0;
+//     for (var i = 0; i < arguments.length; i++) {
+//         rslt += arguments[i];
+//     }
+//     return arguments.length ? (rslt / arguments.length) : 0;
+// }
+// console.log(avg(10,20,30))
+// console.log(avg(...t))
+// punto = { x: 10, y: 20}
+// function coordenadas(x, y) {
+//     return x + y;
+// }
+// coordenadas = (x, y) =>  x + y;
+// console.log(coordenadas(punto.x, punto.y))
+// console.log((coordenadas)(punto.x, punto.y))
+
+// console.log(coordenadas(...punto))
+
+// function suma(a, b) { return a + b; }
+// suma = (a, b) => a + b;
+
+// t.filter(item => item % 2)
+
+// t.filter(item => item > 10)
+// for(let i=0; i < 10; i++)
+//     t.filter(function(item) { return item > a; })
+
+// filtrado = function(item) { return item > 10; }
+
+// for(let i=0; i < 10; i++)
+//     t.filter(filtrado);
+
+
+let tab = new Array();
+tab = [10, 20, 30];
+tab[5] = (a, b) => a + b;
+tab[10] = 'otro'
+tab.push('añade')
+tab.splice(2, 1)
+console.log('Indices')
+for (v in tab) {
+    console.log(v)
 }
+console.log('Valores')
+for (v of tab) {
+    console.log(v)
+}
+// console.log(tab[5](2,3))
 
-console.log(avg(10, 20, 30));
-console.log(avg(...t));
+let o = new Object();
+o = {}
+o.nombre = 'Pepito'
+o['apellidos'] = 'Grillo'
+o.nom = () => this.nombre + ' ' + this.apellidos;
+p = { nombre: 'carmelo', apellidos: 'coton', nom: () => this.nombre + ' ' + this.apellidos }
+o = p;
+console.log(`${o.nombre} ${o.apellidos} ${o.nom()} `)
 
-t.filter(item => item % 2);
+function MiClase(elId, elNombre) {
+    this.id = elId;
+    this.nombre = elNombre;
+    this.muestraId = function () {
+        alert("El ID del objeto es " + this.id);
+    }
+    this.ponNombre = function (nom) {
+        this.nombre = nom.toUpperCase();
+    }
+    return 'Nada'
+}
+MiClase.prototype.cotilla = () => console.log('estoy en el prototipo')
+
+var o1 = new MiClase("99", "Objeto de prueba");
+o1.apellidos = 'Grillo'
+delete o1.nombre
+var o2 = new MiClase("66", "Otro Objeto");
+var otroObjeto = MiClase("99", "Objeto de prueba");
+console.log(o1, o2)
+console.log(otroObjeto)
+o1.cotilla()
+o1.cotilla = () => console.log('estoy en otro prototipo')
+// MiClase.prototype.cotilla = () => console.log('estoy en otro prototipo')
+o2.cotilla()
+o1.cotilla()
